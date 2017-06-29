6fce03fc2075c8caa18b3490e3313f1170a28cc0
Victor Duarte Diniz Monteiro <vddm@cin.ufpe.br>
2017-06-28 23:40:43 -0300

    feature(crawler): normalize data before creating object

--- crawler/extractor.py
+++ crawler/extractor.py
@@ -1,3 +1,4 @@
+import re
 import requests
 from bs4 import BeautifulSoup
 
@@ -5,9 +6,7 @@ from bs4 import BeautifulSoup
 class Extractor(object):
     TABLE_CLS = "product-specs"
     TABLE_ROW_CLS = "product-specs__spec display"
-
-    def __init__(self):
-        self.features_name = [
+    FEATURES_NAME = [
             "marca",
             "linha",
             "modelo",
@@ -22,20 +21,98 @@ class Extractor(object):
         ]
 
     @staticmethod
+    def __match_re(regex_str, group_name, value):
+        match_obj = re.match(regex_str, value)
+        if match_obj:
+            return match_obj.group(group_name)
+        else:
+            raise RuntimeError("No match object for str `{}` with regex `{}`".format(
+                value, regex_str))
+
+    @staticmethod
+    def __normalize_chip_value(value):
+        return Extractor.__match_re("(?P<chip>\w+) chip", "chip", value.lower())
+
+    @staticmethod
+    def __normalize_back_camera(value):
+        return float(Extractor.__match_re(
+            "(?P<camera>\d+([\.,]\d+)?) megapixels",
+            "camera", value.lower()))
+
+    @staticmethod
+    def __normalize_frontal_camera(value):
+        return float(Extractor.__match_re(
+            "(?P<camera>\d+([\.,]\d+)?) megapixels frontal",
+            "camera", value.lower()))
+
+    @staticmethod
+    def __normalize_screen_size(value):
+        return float(Extractor.__match_re(
+            "(?P<size>\d+([\.,]\d+)?)( polegadas)?",
+            "size", value.lower()))
+
+    @staticmethod
+    def __normalize_resolution(value):
+        return Extractor.__match_re(
+            "(?P<resolution>\d+ x \d+) pixels",
+            "resolution", value.lower())
+
+    @staticmethod
+    def __normalize_proccessor(value):
+        return float(Extractor.__match_re(
+            "(?P<proc>\d+([\.,]\d+)?)\ ?ghz",
+            "proc", value.lower()))
+
+    @staticmethod
+    def __normalize_internal_memory(value):
+        return float(Extractor.__match_re(
+            "(?P<mem>\d+([\.,]\d+)?)\ ?gb",
+            "mem", value.lower()))
+    
+    @staticmethod
+    def __normalize_ram_memory(value):
+        return float(Extractor.__match_re(
+            "(?P<mem>\d+([\.,]\d+)?)",
+            "mem", value.lower()))
+
+    @staticmethod
+    def __normalize_value(key, value):
+        key = key.lower()
+        if key == "chips":
+            return Extractor.__normalize_chip_value(value)
+        elif key == "câmera traseira":
+            return Extractor.__normalize_back_camera(value)
+        elif key == "câmera frontal":
+            return Extractor.__normalize_frontal_camera(value)
+        elif key == "tamanho da tela":
+            return Extractor.__normalize_screen_size(value)
+        elif key == "resolução":
+            return Extractor.__normalize_resolution(value)
+        elif key == "velocidade do processador":
+            return Extractor.__normalize_proccessor(value)
+        elif key == "memória interna":
+            return Extractor.__normalize_internal_memory(value)
+        elif key == "memória ram":
+            return Extractor.__normalize_ram_memory(value)
+        else:
+            return value
+
+    @staticmethod
     def extract_page_as_bs(url):
         request_page = requests.get(url)
         return BeautifulSoup(request_page.text, "html.parser")
 
-    def extract_obj(self, soup_obj):
-        table = soup_obj.find("table", class_=self.TABLE_CLS)
-        t_row = table.tbody.find_all("tr", class_=self.TABLE_ROW_CLS)
+    @classmethod
+    def extract_obj(cls, soup_obj):
+        table = soup_obj.find("table", class_=cls.TABLE_CLS)
+        t_row = table.tbody.find_all("tr", class_=cls.TABLE_ROW_CLS)
         final_obj = {}
 
         for row in t_row:
             data = row.find_all("td")
             key = str(data[0].string)
             value = str(data[1].string)
-            if key.lower() in self.features_name:
-                final_obj[key] = value
+            if key.lower() in cls.FEATURES_NAME:
+                final_obj[key] = cls.__normalize_value(key, value)
 
         return final_obj
