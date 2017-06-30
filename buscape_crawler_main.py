191d4f92f304dae00028a0f539ce7cb0dfc17474
Victor Duarte Diniz Monteiro <vddm@cin.ufpe.br>
2017-06-29 23:49:47 -0300

    fix(crawler.runnable): choose good value for final page

--- buscape_crawler_main.py
+++ buscape_crawler_main.py
@@ -2,7 +2,7 @@ from crawler.buscape_crawler import BuscapeCrawler
 import os
 
 if __name__ == '__main__':
-    crawler = BuscapeCrawler(1, 2)
+    crawler = BuscapeCrawler(1, 10)
     crawler.save_into_csv(
         path=os.path.join(os.getcwd(), "output", "data.csv"),
         objs=crawler.run_crawler())
