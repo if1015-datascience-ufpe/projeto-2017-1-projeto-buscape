73358188e8f2fce832e89d20174134858ecf785f
Victor Duarte Diniz Monteiro <vddm@cin.ufpe.br>
2017-06-29 23:47:35 -0300

    feature(crawler): logging support

--- crawler/buscape_crawler.py
+++ crawler/buscape_crawler.py
@@ -1,5 +1,9 @@
 import time
 
+import logging
+
+import sys
+
 from crawler.csv_writer import CSVWriter
 from crawler.extractor import Extractor
 
@@ -8,6 +12,7 @@ class BuscapeCrawler(object):
     LIST_PAGE_URL = "http://www.buscape.com.br/celular-e-smartphone?"
     DATA_CLASS = "bui-product__link--outer"
     DATA_GAACTION = "Card de Produto"
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
     def __init__(self, start_page_number, last_page_number):
         self.__cur_page = start_page_number
@@ -16,6 +21,8 @@ class BuscapeCrawler(object):
         self.__last_page = last_page_number
         self.__update_list()
         self.__extractor = Extractor()
+        self.__logger = logging.root.getChild(self.__class__.__module__ + "." +
+                                              self.__class__.__name__)
 
     def __get_link(self):
         bs4_obj = self.__cur_list[self.__cur_item]
@@ -40,6 +47,7 @@ class BuscapeCrawler(object):
                 self.__update_list()
                 item = self.__next_item()
 
+        self.__logger.info("Page: {} \ item: {}".format(self.__cur_page, self.__cur_item))
         return item
 
     def run_crawler(self):
@@ -47,6 +55,7 @@ class BuscapeCrawler(object):
         final_items = []
 
         while item_link is not None:
+            self.__logger.info("Capturando item do link: {}". format(item_link))
             bs4_obj = self.__extractor.extract_page_as_bs(item_link)
             final_items.append(self.__extractor.extract_obj(bs4_obj))
 
