0a775dfbfa9b9e606741fa408d64850f606e9c26
Victor Duarte Diniz Monteiro <vddm@cin.ufpe.br>
2017-07-01 09:19:24 -0300

    feature(crawler): add price extraction, logging and refactor
    * get price from tag `a` in items list page not in item details page
    * logging file to easy debbug application
    * run crawler until it has run all pages, not until page has no item

--- buscape_crawler_main.py
+++ buscape_crawler_main.py
@@ -1,8 +1,14 @@
 from crawler.buscape_crawler import BuscapeCrawler
 import os
 
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("final_page", type=int)
+args = parser.parse_args()
+
 if __name__ == '__main__':
-    crawler = BuscapeCrawler(1, 10)
+    crawler = BuscapeCrawler(1, args.final_page)
     crawler.save_into_csv(
         path=os.path.join(os.getcwd(), "output", "data.csv"),
         objs=crawler.run_crawler())
--- crawler/buscape_crawler.py
+++ crawler/buscape_crawler.py
@@ -1,22 +1,22 @@
+import os
 import time
 
 import logging
 
-import sys
-
 from crawler.csv_writer import CSVWriter
-from crawler.extractor import Extractor
+from crawler.extractor import Extractor, HTMLFormationNotImplemented
 
 
 class BuscapeCrawler(object):
     LIST_PAGE_URL = "http://www.buscape.com.br/celular-e-smartphone?"
     DATA_CLASS = "bui-product__link--outer"
     DATA_GAACTION = "Card de Produto"
-    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+    logging.basicConfig(filename=os.path.join(os.getcwd(), "crawler.log"),
+                        level=logging.INFO)
 
     def __init__(self, start_page_number, last_page_number):
         self.__cur_page = start_page_number
-        self.__cur_item = 0
+        self.__cur_item = -1
         self.__cur_list = None
         self.__last_page = last_page_number
         self.__update_list()
@@ -34,33 +34,53 @@ class BuscapeCrawler(object):
         self.__cur_list = bs4_obj.find_all("a",
                                            class_=self.DATA_CLASS,
                                            attrs={"data-gaaction": self.DATA_GAACTION})
-        self.__cur_item = 0
+        self.__cur_item = -1
 
     def __next_item(self):
         item = None
         if self.__cur_page <= self.__last_page:
-            if self.__cur_item < len(self.__cur_list):
-                item = self.__get_link()
+            if self.__cur_item + 1 < len(self.__cur_list):
                 self.__cur_item += 1
+                item = self.__get_link()
+                self.__logger.info("Item #{}: {}".format(self.__cur_item, item))
             else:
                 self.__cur_page += 1
+                self.__logger.info("Page number: {}.".format(self.__cur_page))
                 self.__update_list()
                 item = self.__next_item()
 
-        self.__logger.info("Page: {} \ item: {}".format(self.__cur_page, self.__cur_item))
         return item
 
+    def __get_price(self):
+        bs4_obj = self.__cur_list[self.__cur_item]
+        try:
+            return float(bs4_obj.get("data-preco", -1))
+        except ValueError:
+            self.__logger.warning("Não foi possível extrair preço do item {}.".format(
+                self.__get_link()
+            ))
+            return ""
+
     def run_crawler(self):
-        item_link = self.__next_item()
+        logging.info("Running crawler from page 1 to {}".format(self.__last_page))
         final_items = []
 
-        while item_link is not None:
-            self.__logger.info("Capturando item do link: {}". format(item_link))
-            bs4_obj = self.__extractor.extract_page_as_bs(item_link)
-            final_items.append(self.__extractor.extract_obj(bs4_obj))
-
+        while self.__cur_page <= self.__last_page:
             item_link = self.__next_item()
-            time.sleep(0.5)
+            if item_link:
+                self.__logger.info("Capturando item do link: {}". format(item_link))
+                bs4_obj = self.__extractor.extract_page_as_bs(item_link)
+                try:
+                    new_item = self.__extractor.extract_obj(bs4_obj)
+                    new_item["url"] = item_link
+                    new_item["preco"] = self.__get_price()
+                    final_items.append(new_item)
+                except HTMLFormationNotImplemented:
+                    self.__logger.warning("Formato de HTML da link {} não suportado.".format(
+                        item_link
+                    ))
+
+            time.sleep(1)
 
         return final_items
 
